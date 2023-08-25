7fff4a8ffe6017f39b88117f294fe3d3cb09dad2
berkersaptas <berkersaptas@gmail.com>
2023-08-25 22:03:43 +0300

    Initial Commit

---
+++ FluxStore/FluxStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxStoreApp.swift
+//  FluxStore
+//
+//  Created by Berker Saptas on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FluxStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
